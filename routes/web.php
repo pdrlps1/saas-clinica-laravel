91c9e7aa3f6cb7c91ce89219d4c041600729e257
pdrlps1 <pedro.otavio-12@outlook.com>
2025-11-14 16:24:33 -0300

    feat: Routes update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Http\Controllers\AppointmentController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\OrganizationController;
+use App\Http\Controllers\PatientController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+//Rotas públicas
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+
+//Rotas autenticadas
+Route::middleware('auth')->group(function() {
+
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+
+    Route::resource('organizations', OrganizationController::class);
+    Route::resource('patients', PatientController::class);
+    Route::resource('appointments', AppointmentController::class);
+
+    Route::patch('/appointments/{appointment}/complete', [AppointmentController::class, 'complete'])->name('appointments.complete');
+    Route::patch('/appointments/{appointment}/cancel', [AppointmentController::class, 'cancel'])->name('appointments.cancel');
 });
